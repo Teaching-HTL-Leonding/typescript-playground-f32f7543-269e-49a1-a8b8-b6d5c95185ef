a97bce888ce8fdf67dc1995b4c18251e31352d8a
Rainer Stropek <rainer@timecockpit.com>
2026-09-25 10:13:11 +0200

    Saved in TypeScript Playground at 2026-09-25T08:13:10.906Z

--- house/index.ts
+++ house/index.ts
@@ -4,14 +4,24 @@ function setup() {
     background("skyblue")
     stroke("black");
     strokeWeight(1);
+
+    // create walls
     fill("red")
     rect(100,300,175,200)
+
+    // create door
     fill("yellow")
-    rect(163,425,50,75)
+    rect(163,425,50,80)
+
+    // create roof
     fill("darkred")
     triangle(90,300,285,300,187.5,150)
+
+    //create tree log
     fill("brown")
     rect(325,330,45,175)
+
+    // create tree leaves
     fill("darkgreen")
     circle(325,290,100)
     circle(370,290, 100)
