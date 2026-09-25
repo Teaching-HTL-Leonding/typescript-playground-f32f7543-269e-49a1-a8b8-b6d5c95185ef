614d35bd70162a0baa6b71637fd62af88212751e
Rainer Stropek <rainer@timecockpit.com>
2026-09-25 09:01:42 +0200

    Saved in TypeScript Playground at 2026-09-25T07:01:41.974Z

---
+++ house/index.ts
@@ -0,0 +1,9 @@
+function setup() {
+    //create canva
+    createCanvas(500,500);
+    background("skyblue")
+    stroke("white");
+    strokeWeight(10);
+    fill("yellow")
+    rect(100,100,500)
+}
