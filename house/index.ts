15e4db178b4a92212c237cca15d5fa0b39429dd4
Rainer Stropek <rainer@timecockpit.com>
2026-09-25 10:04:45 +0200

    Saved in TypeScript Playground at 2026-09-25T08:04:44.688Z

--- house/index.ts
+++ house/index.ts
@@ -2,8 +2,18 @@ function setup() {
     //create canva
     createCanvas(500,500);
     background("skyblue")
-    stroke("white");
-    strokeWeight(10);
+    stroke("black");
+    strokeWeight(1);
+    fill("red")
+    rect(100,300,175,200)
     fill("yellow")
-    rect(100,100,500)
+    rect(156,425,50,75)
+    fill("darkred")
+    triangle(90,300,285,300,187.5,150)
+    fill("brown")
+    rect(325,330,45,175)
+    fill("darkgreen")
+    circle(325,290,100)
+    circle(370,290, 100)
+    circle(348,270,100)
 }
